3171ddb622a50d4897122c8aa54b71095be38516
Umar <umahmud@outlook.com>
2015-08-30 12:43:06 -0400

    work in progress

--- Unit-1-Assessment/MyTableViewController.h
+++ Unit-1-Assessment/MyTableViewController.h
@@ -1,15 +1,13 @@
 //
-//  ViewController.h
+//  MyTableViewController.h
 //  Unit-1-Assessment
 //
-//  Created by 👨🏻 on 8/21/15.
+//  Created by Umar on 8/30/15.
 //  Copyright (c) 2015 Mike Kavouras. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface MyTableViewController : UITableViewController
 
 @end
-
