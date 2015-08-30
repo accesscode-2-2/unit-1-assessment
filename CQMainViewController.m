79380b15201e3e8c2f9443c6686c370b772a1af7
ayunav <ayunanyc@gmail.com>
2015-08-30 11:50:06 -0400

    first commit

---
+++ CQMainViewController.m
@@ -0,0 +1,40 @@
+//
+//  CQMainViewController.m
+//  Unit-1-Assessment
+//
+//  Created by Ayuna Vogel on 8/30/15.
+//  Copyright (c) 2015 Mike Kavouras. All rights reserved.
+//
+
+#import "CQMainViewController.h"
+
+@interface CQMainViewController ()
+
+@end
+
+@implementation CQMainViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+//    The + button should increase the number in the label by 1
+//    The - button should decrease the number in the label by 1
+//    The push button should transition to the table view controller
+//    The delay push button should transition to the table view controller after a 2 second delay
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
---
+++ Unit-1-Assessment/CQDevsTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  CQDevsTableViewController.h
+//  Unit-1-Assessment
+//
+//  Created by Ayuna Vogel on 8/30/15.
+//  Copyright (c) 2015 Mike Kavouras. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CQDevsTableViewController : UITableViewController
+
+@end
