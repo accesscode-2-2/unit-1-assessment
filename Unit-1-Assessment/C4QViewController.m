fb469c2e2d05efc5be9eb15189886d69326ca588
Jackie Meggesto <jackie.meggesto@gmail.com>
2015-08-30 11:37:11 -0400

    all tests passing, all app requirements satisfied

---
+++ Unit-1-Assessment/C4QViewController.m
@@ -0,0 +1,33 @@
+//
+//  C4QViewController.m
+//  Unit-1-Assessment
+//
+//  Created by Jackie Meggesto on 8/30/15.
+//  Copyright (c) 2015 Mike Kavouras. All rights reserved.
+//
+
+#import "C4QViewController.h"
+
+@interface C4QViewController ()
+
+@end
+
+@implementation C4QViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    UIImage *image = [UIImage imageNamed:self.formattedString];
+    [self.DevImage setImage:image];
+    self.navigationItem.title = self.originalString;
+    
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+
+@end
--- Unit-1-Assessment/ViewController.m
+++ Unit-1-Assessment/ViewController.m
@@ -7,17 +7,44 @@
 //
 
 #import "ViewController.h"
+#import "C4QTableViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UILabel *incrementLabel;
+@property (nonatomic) NSTimer* delayPush;
 
 @end
 
 @implementation ViewController
 
+-(void)setUpDelayPush {
+    self.delayPush = [NSTimer timerWithTimeInterval:2.0 target:self selector:@selector(fireDelayPushTimer) userInfo:nil repeats:NO];
+    [[NSRunLoop mainRunLoop] addTimer:self.delayPush forMode:NSRunLoopCommonModes];
+}
 - (void)viewDidLoad {
     [super viewDidLoad];
-   
-    // do stuff
+    
+    
 }
+- (IBAction)plusButton:(id)sender {
+    self.incrementLabel.text = [NSString stringWithFormat:@"%d", [self.incrementLabel.text intValue] +1];
+}
+- (IBAction)minusButton:(id)sender {
+ self.incrementLabel.text = [NSString stringWithFormat:@"%d", [self.incrementLabel.text intValue] -1];
+}
+-(void)fireDelayPushTimer {
+    C4QTableViewController *pushTable = [self.storyboard instantiateViewControllerWithIdentifier:@"C4QTableViewController"];
+    
+    [self.navigationController pushViewController:pushTable animated:YES];
+}
+- (IBAction)pushButton:(id)sender {
+    C4QTableViewController *pushTable = [self.storyboard instantiateViewControllerWithIdentifier:@"C4QTableViewController"];
+    
+    [self.navigationController pushViewController:pushTable animated:YES];
+}
+- (IBAction)delayPushButton:(id)sender {
+    [self setUpDelayPush];
+}
+
 
 @end
