5feb17c9cf6708eb74492f3fea31cc97e222a466
cdolm92 <cdolm92@gmail.com>
2015-08-30 13:56:14 -0400

    added developer objects ^-^

---
+++ Unit-1-Assessment/Developers.h
@@ -0,0 +1,13 @@
+//
+//  Developers.h
+//  Unit-1-Assessment
+//
+//  Created by Christella on 8/30/15.
+//  Copyright (c) 2015 Mike Kavouras. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Developers : NSObject
+
+@end
---
+++ Unit-1-Assessment/Developers.m
@@ -0,0 +1,13 @@
+//
+//  Developers.m
+//  Unit-1-Assessment
+//
+//  Created by Christella on 8/30/15.
+//  Copyright (c) 2015 Mike Kavouras. All rights reserved.
+//
+
+#import "Developers.h"
+
+@implementation Developers
+
+@end
