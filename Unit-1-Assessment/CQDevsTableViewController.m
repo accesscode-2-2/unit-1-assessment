b21754842c2799e7ae8238903a1616d4311b7eda
ayunav <ayunanyc@gmail.com>
2015-08-30 13:15:31 -0400

    added nagivation item title in the table vc.

--- Unit-1-Assessment/CQDevsTableViewController.m
+++ Unit-1-Assessment/CQDevsTableViewController.m
@@ -26,6 +26,8 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
+    self.navigationItem.title = @"Access Code 2.2";
+    
     self.developers = @[
                         @"Lauren Caponong",
                         @"Artur Lan",
