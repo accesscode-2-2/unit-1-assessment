d228d030a162662ca05db6f0bfe1983bce2c4920
cdolm92 <cdolm92@gmail.com>
2015-08-30 13:51:33 -0400

    added tvc object

---
+++ Unit-1-Assessment/DevsTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  DevsTableViewController.h
+//  Unit-1-Assessment
+//
+//  Created by Christella on 8/30/15.
+//  Copyright (c) 2015 Mike Kavouras. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DevsTableViewController : UITableViewController
+
+@end
