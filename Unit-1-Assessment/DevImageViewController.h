659d84a3f301a38f9cfcb906ce5047f5f29ecb76
cdolm92 <cdolm92@gmail.com>
2015-08-30 14:46:21 -0400

    working on images

---
+++ Unit-1-Assessment/DevImageViewController.h
@@ -0,0 +1,15 @@
+//
+//  DevImageViewController.h
+//  Unit-1-Assessment
+//
+//  Created by Christella on 8/30/15.
+//  Copyright (c) 2015 Mike Kavouras. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DevImageViewController : UIViewController
+
+@property (nonatomic) NSString *devName;
+
+@end
---
+++ Unit-1-Assessment/DevImageViewController.m
@@ -0,0 +1,35 @@
+//
+//  DevImageViewController.m
+//  Unit-1-Assessment
+//
+//  Created by Christella on 8/30/15.
+//  Copyright (c) 2015 Mike Kavouras. All rights reserved.
+//
+
+#import "DevImageViewController.h"
+
+@interface DevImageViewController ()
+
+@property (weak, nonatomic) IBOutlet UIImageView *devPic;
+
+@end
+
+@implementation DevImageViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    NSString *imageName = [self.devName lowercaseString];
+    self.devPic.image = [UIImage imageNamed:imageName];
+    
+    self.navigationItem.title = self.pokemonName;
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+
+@end
